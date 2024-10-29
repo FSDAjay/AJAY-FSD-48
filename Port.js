c08eb1ab430cc70a76af94c5b713a24f8d0fd55d
FSDAjay <ajayfullstack0241@gmail.com>
2024-10-29 10:31:52 +0530

    Add files via upload
    Advanced portfolio

---
+++ Port.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $(".form-check-input").click(function () {
+        $(".main").toggleClass("Up-Slide22");
+        $(".record").toggleClass("record2");
+        $(".page").toggleClass("page22");
+        $(".status").toggleClass("status-h22");
+        $(".About-h3 ").toggleClass("About-h32t");
+        $(".skills-h3").toggleClass("skills-h32t");
+        $(".services-h3").toggleClass("services-h3t");
+        $(".cont-h3").toggleClass("cont-h32t");
+        $(".cont").toggleClass("container2");
+
+
+
+    });
+
+
+});
